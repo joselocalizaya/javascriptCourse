f60b6a45ada9a1fa5acbc89401ba91b6e0e1cb72
joselocalizaya <grafitek5tudio@outlook.com>
2023-03-17 00:18:03 +0100

    cuarta commit

---
+++ date.js
@@ -0,0 +1,7 @@
+var date = new Date();
+
+var year = date.getFullYear();
+var month = date.getMonth();
+var day = date.getDate();
+
+console.log("Hola hoy es " + day + " del mes " + month + " del año " + year);
